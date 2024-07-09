1db22357dbf408cae5ce256a85ee4678d7445223
belarminon <35656300+belarminon@users.noreply.github.com>
2024-07-09 22:08:55 +0000

    merge com a main

---
+++ dist/auth/users/users.controller.d.ts
@@ -0,0 +1,6 @@
+import { UsersService } from './users.service';
+export declare class UsersController {
+    private usersService;
+    constructor(usersService: UsersService);
+    create(data: any): void;
+}
